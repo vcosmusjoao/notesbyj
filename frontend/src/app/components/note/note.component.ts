d45df1e55c47341334e4bab5eed25a59840f9ce4
vcosmusjoao <joaodevcosta@gmail.com>
2023-04-07 20:23:44 -0300

    Criação modal para novas notas

--- frontend/src/app/components/note/note.component.ts
+++ frontend/src/app/components/note/note.component.ts
@@ -1,6 +1,8 @@
+import { DialogService } from 'primeng/dynamicdialog';
 import { Note } from './../../models/note';
 import { NoteService } from './../../services/note.service';
 import { Component, OnInit } from '@angular/core';
+import { CreateNoteModalComponent } from '../create-note-modal/create-note-modal.component';
 
 @Component({
   selector: 'app-note',
@@ -11,12 +13,26 @@ export class NoteComponent implements OnInit {
 
   notes: Note[] = [];
 
-  constructor(private noteService: NoteService) { }
+  constructor(private noteService: NoteService, public dialogService: DialogService) { }
 
   ngOnInit(): void {
+   this.getAllNotes();
+  }
+
+  getAllNotes(){
     this.noteService.getAllNotes().subscribe(response =>{
       this.notes = response.content;
     });
+  }
+
+  createNoteModal(){
+    const ref = this.dialogService.open(CreateNoteModalComponent,{
+      width:'40%',
+      height:'70%'
+    });
+    ref.onClose.subscribe(()=>{
+      this.getAllNotes();
+    })
   }
 
 }
