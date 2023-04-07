0b1a802953f139a96bd725452383b9a2e1bebc32
vcosmusjoao <joaodevcosta@gmail.com>
2023-04-07 20:22:51 -0300

    Criando endpoint para registrar nova Nota

--- frontend/src/app/services/note.service.ts
+++ frontend/src/app/services/note.service.ts
@@ -2,6 +2,7 @@ import { Note } from './../models/note';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { url } from 'inspector';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +16,10 @@ export class NoteService {
 
   getAllNotes(): Observable<any>{
     return this.httpClientService.get(this.baseUrl);
+  }
+
+  createNote(note: Note): Observable<any> {
+    return this.httpClientService.post(this.baseUrl, note);
   }
 
 }
