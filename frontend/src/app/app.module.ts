b9c3a0aa98014b05cc7b08f1b1b077e0b4edefec
vcosmusjoao <joaodevcosta@gmail.com>
2023-04-07 20:22:37 -0300

    Importando novos modulos do primeng para modal funcionar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,22 +1,33 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { DialogModule } from 'primeng/dialog';
+import { DynamicDialogModule, DialogService } from 'primeng/dynamicdialog';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoteComponent } from './components/note/note.component';
+import { CreateNoteModalComponent } from './components/create-note-modal/create-note-modal.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NoteComponent
+    NoteComponent,
+    CreateNoteModalComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    DialogModule,
+    DynamicDialogModule,
+    CommonModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [DialogService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
